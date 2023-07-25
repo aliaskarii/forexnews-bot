e6201966e660d32299568710f99d9d68c2a385e6
aliaskari76 <askari1791@hotmail.com>
2023-07-25 13:05:08 -0400

    improve telegram-bot

--- telegram-bot/src/main.rs
+++ telegram-bot/src/main.rs
@@ -1,22 +1,25 @@
 use teloxide::{prelude::*};
 use pretty_env_logger::env_logger::from_env;
 
-
 #[tokio::main]
-async fn telegram(txt_msg:String) {
-
-        match dotenvy::dotenv() {
-            Ok(path) => println!(".env read successfully from {}", path.display()),
-            Err(e) => println!("Could not load .env file: {e}"),
-        };
-   
-
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    match dotenvy::dotenv() {
+        Ok(path) => println!(".env read successfully from {}", path.display()),
+        Err(e) => println!("Could not load .env file: {e}"),
+    };
     pretty_env_logger::init();
     log::info!("Starting News bot...");
-    let bot = Bot::from_env();
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_message(msg.chat.id,txt_msg).await?;
-        Ok(())
-    })
-    .await;
+    let your_id = 196176954;
+    let bot = Bot::from_env().auto_send();
+    teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
+        if let Some(text) = message.text() {
+            bot.send_message(message.chat.id, text).await?;
+        }
+        respond(())
+    }).await;
+    Ok(())
 }
+
+
+
+   
