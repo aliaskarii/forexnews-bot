4946aae26d3e6617f327ee410a6dfb9e86a40c7d
aliaskari76 <askari1791@hotmail.com>
2023-07-25 14:25:28 -0400

    impl

--- telegram-bot/src/main.rs
+++ telegram-bot/src/main.rs
@@ -1,6 +1,6 @@
 use teloxide::prelude::{*, ChatId};
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main(text:String) -> Result<(), Box<dyn std::error::Error>> {
     match dotenvy::dotenv() {
         Ok(path) => println!(".env read successfully from {}", path.display()),
         Err(e) => println!("Could not load .env file: {e}"),
@@ -8,10 +8,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
     log::info!("Starting News bot...");
     let your_id = ChatId(196176954);
-    let text_msg = "test";
     let bot = Bot::from_env();
     bot
-    .send_message(your_id, text_msg)
+    .send_message(your_id, text)
     .protect_content(true) // <-- optional parameter!
     .await?;
     Ok(())
