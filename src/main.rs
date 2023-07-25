ec6914d1e16dd3a46636b561c2d69d3e41aaa6e3
aliaskari76 <askari1791@hotmail.com>
2023-07-25 09:08:44 -0400

    adding telegram message sender

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use minreq::{Response, Error, Request};
+use pretty_env_logger::env_logger::from_env;
 use serde::Deserialize;
+use teloxide::{prelude::*};
 
 #[derive(Debug, Deserialize)]
 struct NewsItem {
@@ -16,12 +19,39 @@ struct NewsItem {
 
 const URL: &str = "https://nfs.faireconomy.media/ff_calendar_thisweek.json?version=e7ef4a21d0d488b886475f77d0ca5806";
 
+#[tokio::main]
+async fn telegram(txt_msg:String) {
+
+        match dotenvy::dotenv() {
+            Ok(path) => println!(".env read successfully from {}", path.display()),
+            Err(e) => println!("Could not load .env file: {e}"),
+        };
+   
+
+    pretty_env_logger::init();
+    log::info!("Starting News bot...");
+    let bot = Bot::from_env();
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_message(msg.chat.id,txt_msg).await?;
+        Ok(())
+    })
+    .await;
+}
+
+
 fn main() {
-    let response = minreq::get(URL).with_timeout(8).send().unwrap();
+    let json_result:Result<Response, Error> = minreq::get(URL).with_timeout(10).send();
+    let response =  match json_result {
+        Ok(get) => get , 
+        Err(error) => panic!("{}", error),
+    };
     let news: Vec<NewsItem> = serde_json::from_str(response.as_str().unwrap()).unwrap();
+
+
     news
         .iter()
         .filter(|x| x.impact == "High")
-        .map(|x| &x.title)
-        .for_each(|t| println!("title: {title}", title=t));
+        .for_each(|t| println!("{:#?}",t));
+
+   
 }
